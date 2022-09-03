f737c54904034c29e49821db4950b7fb0a137bb4
Valeriy Semko <valsemko74@gmail.com>
2022-09-03 10:59:50 +0300

    remake App to hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { fetchImages } from '../../src/servises/pixabayApi';
 import { Box } from 'components/Box';
 import { Loader } from 'components/Loader/Loader';
@@ -13,6 +13,7 @@ import { Button } from 'components/Button/Button';
 
 export const App = () => {
 
+  const isMounted = useRef(false);
   const [images, setImages] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
   const [page, setPage] = useState(1);
@@ -31,14 +32,17 @@ export const App = () => {
   }, [searchQuery]);
 
   useEffect(() => {
-    fetchNewImages(searchQuery, page);
-  }, [page, searchQuery]);
+    if (isMounted.current) {
+      fetchNewImages(searchQuery, page);
+    }
+    isMounted.current = true;
+  }, [ page, searchQuery]);
 
   const handleFormSubmit = searchQuery => {
      setSearchQuery( searchQuery );
   }
   
-    const fetchNewImages = async (searchQuery = '', page = 1) => {
+  const fetchNewImages = async (searchQuery = '', page = 1) => {
     try {
       setIsLoading(true);
 
@@ -60,7 +64,7 @@ export const App = () => {
     } finally {
       setIsLoading(false);
     }
-    };
+  };
   
     const openModal = ( tags, largeImageURL) => {
       setLargeImageURL(largeImageURL);
@@ -94,97 +98,4 @@ export const App = () => {
     </Box>
   );
 } 
-//   state = {
-//     images: [],
-//     searchQuery: '',
-//     page: 1,
-//     error: '',
-//     isLoading: false,
-//     showModal: false,
-//     largeImageURL: '',
-//     tags: '',
-//   };
 
-//   componentDidUpdate(_, prevState) {
-//     const { searchQuery, page } = this.state;
-//     if (prevState.searchQuery !== this.state.searchQuery) {
-//       this.setState({
-        
-//         images: [],
-//         searchQuery,
-//         page: 1,
-//         error: '',
-//         showModal: false,
-//       });
-//     }
-//     if (prevState.searchQuery !== this.state.searchQuery && page !== 1) {
-//       return
-//     }
-//     if (prevState.searchQuery !== this.state.searchQuery || prevState.page !== page) {
-//       this.fetchNewImages(searchQuery, page);
-//         }
-//     }
-  
-  
-//   hendleFormSubmit = searchQuery => {
-//     this.setState({ searchQuery });
-//   }
-
-  // fetchNewImages = async (searchQuery = '', page = 1) => {
-  //   try {
-  //     this.setState({ isLoading: true });
-  //     const responceImages = await fetchImages(searchQuery, page);
-
-  //     if (responceImages.length === 0) {
-  //       throw new Error(`No result on request ${searchQuery}`)
-  //     }
-  //     const newImages = responceImages.map(
-  //       ({ id, webformatURL, largeImageURL, tags }) => ({
-  //         id, webformatURL, largeImageURL, tags,
-  //       })
-  //     );
-  //     this.setState(prevState => ({
-  //       images: [...prevState.images, ...newImages],
-  //     }));
-  //   } catch (error) {
-  //     this.setState({
-  //       error: error.message,
-  //     });
-  //   } finally {
-  //     this.setState({ isLoading: false });
-  //   }
-  // };
-
-  // openModal = ( tags, largeImageURL) => {
-  //   this.setState({ largeImageURL, tags, showModal: true });
-  // };
-
-  
-
-  // onClickLoadMoreBtn = () => {
-  //   this.setState(prevState => ({
-  //     page: prevState.page + 1,
-  //   }))
-  // };
-  
-//   render() {
-//     const { isLoading, images, largeImageURL, tags } = this.state;
-    
-//     return (
-//       <Box>      
-//         <Searchbar onFormSubmit={this.hendleFormSubmit}>Gallery</Searchbar>
-//         {images.length > 0 && 
-//           <>
-//             <ImageGallery images={images} openModal={this.openModal} />
-//             <Button handleClick={this.onClickLoadMoreBtn} />
-//           </>}
-//         {isLoading && <Loader />}
-//         {this.state.showModal && (
-//           <Modal onClose={this.closeModal} >
-//             <ModalImage src={largeImageURL} alt={tags} />
-//           </Modal>
-//           )}
-//       </Box>
-//     );
-// //   }
-// // }
